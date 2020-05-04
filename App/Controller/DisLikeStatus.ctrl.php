f4716d4042a6e548b683e4af36cd71f28711e8d2
ING.steeve <steevesanon6@gmail.com>
2020-05-03 21:04:52 -0400

    Online Version Done For today

--- App/Controller/DisLikeStatus.ctrl.php
+++ App/Controller/DisLikeStatus.ctrl.php
@@ -3,7 +3,7 @@ require_once '../init.php';
 use App\Model\Status;
 if(!empty($_GET['id_user']) AND !empty($_GET['id_post'])){
     Status::Disike($_GET['id_user'],$_GET['id_post']);
-    //var_dump($_SERVER);
+    $id=$_GET['id'];
 
-    header('Location:'.$_SERVER['HTTP_REFERER']);
+    header("Location:https://online-doctorapp.000webhostapp.com/Pages/Profile.php?id=.$id.");
 }
